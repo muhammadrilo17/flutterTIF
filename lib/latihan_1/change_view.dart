dd6bd44c952342144c8a0b2a247919844aa5b96a
dikynugraha1111 <diky1800018380@webmail.uad.ac.id>
2021-12-08 13:06:13 +0700

    Latihan 1

--- lib/latihan_1/change_view.dart
+++ lib/latihan_1/change_view.dart
@@ -8,16 +8,20 @@ class ChangeView extends StatefulWidget {
 }
 
 class _ChangeViewState extends State<ChangeView> {
+  // Mendefinisikan nilai awal pada private variable setData
   String _setData = "Old Text";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        margin: const EdgeInsets.all(24),
+        // margin pada container(Pelapis) digunakan untuk memberi jarak dengan layout dasar/ utama.
+        margin: const EdgeInsets.all(24.0),
         child: Center(
           child: Column(
             children: [
+              // Container sebagai widget lapisan. Bisa dilakukan decoration dengan BoxDecoration mulai dari warna,
+              //radius, image, shadow dsb.
               Container(
                 decoration: const BoxDecoration(
                   color: Color(0xff000000),
@@ -32,8 +36,10 @@ class _ChangeViewState extends State<ChangeView> {
                   ),
                 ),
               ),
+              // Menampilkan Button yang mana terdapat triger di parameter onPressednya.
               ElevatedButton(
                 onPressed: () {
+                  // setState digunakan untuk me refresh/ reload halaman ketika terjadi suatu perubahan.
                   setState(() {
                     _setData = "New Data";
                   });
---
+++ lib/latihan_1/readme.md
@@ -0,0 +1,8 @@
+# Latihan 1
+
+Pada latihan 1 ini berisikan tentang bagaimana jalannya aplikasi pada flutter.
+
+- Untuk home.dart, berisikan tampilan untuk memunculkan text di center layout.
+- Untuk change_view.dart, berisikan bagaimana kita memahami akan statefull widget / State yang dapat berubah ubah secara dinamis.
+
+Untuk melihat setiap penjelasan fitur. Silahkan langsung cek di File masing". Terdapat penjelasan komentar di setiap line codenya.
